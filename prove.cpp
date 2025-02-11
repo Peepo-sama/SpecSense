875a16a1092f71847b13fc12f9f98f670666cbbe
Peepo-sama <117993293+Peepo-sama@users.noreply.github.com>
2025-02-11 23:16:39 +0100

    Update prove.cpp
    Incluso tutti i tipi di processori AMD ryzen 3

--- prove.cpp
+++ prove.cpp
@@ -22,6 +22,16 @@ void getMemoryInfo() {
 
 
 
+
+
+case "1200" || "1300X" || "2300X" || "3200G" || "3300X" || "3900X" || "3950X" || "5600X" || "5800X" || "5900X" || "5950X" || "5950X" || "7440U" || "7320U" || "210" || "8440U":   //per tutti i tipi di processori amd ryzen 3
+
+
+
+
+
+
+
 int main() {
     getCPUInfo();
     getMemoryInfo();
